4701f178bee95a006b390596707e0950ec29c554
gopika0607 <dgopika0607@gmail.com>
2026-02-12 19:46:33 +0530

    Time: 5596 ms (14.79%) | Memory: 19.4 MB (44.33%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        max_len = 0
+        
+        for i in range(n):
+            freq = [0] * 26
+            
+            for j in range(i, n):
+                freq[ord(s[j]) - ord('a')] += 1
+                
+                counts = [f for f in freq if f > 0]
+                
+                if len(set(counts)) == 1:
+                    max_len = max(max_len, j - i + 1)
+        
+        return max_len
